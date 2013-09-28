fb3875d4a5228a69a4c315a6d5d4a140dc4f5a93
Oskar Thoren <o.thoren@gmail.com>
2013-09-28 16:18:31 +0200

    remove stuff

--- src/hnfollow/core.clj
+++
@@ -1,6 +0,0 @@
-(ns followhackers.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/hnfollow/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns hnfollow.core-test
-  (:use clojure.test
-        hnfollow.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
