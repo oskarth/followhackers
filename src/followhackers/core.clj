1cdde392c0e2d6a491f2fa7a14a8022a0d5052a6
Oskar Thoren <o.thoren@gmail.com>
2013-09-29 12:00:18 +0200

    add db; sort by date

--- src/followhackers/core.clj
+++ src/followhackers/core.clj
@@ -11,20 +11,39 @@
             [compojure.route :as route]
             [org.httpkit.client :as http]))
 
-;; TODO: search for multiple users at once
 ;; TODO: sort by last day
+
 ;; TODO: assoc/dissoc responses
 ;; TODO: postal for email
 ;; (def f (future (Thread/sleep 10000) (println "done") 100))
 ;; TODO: commit and tag release
+;; TODO: search for multiple users at once
+;; TODO: feedback thingy
+;; TODO: vote for us flerp?
+
+;; data
+
+(def celebs (atom {"pg" "all pg text"}))
+(defn save-celebs! [] (spit "data/celebs.db" (prn-str @celebs)))
+(defn load-celebs! [] (reset! celebs (read-string (slurp "data/celebs.db"))))
+
+(def fans (atom {"me@oskarth.com" ["pg grellas"]}))
+(defn save-fans! [] (spit "data/fans.db" (prn-str @fans)))
+(defn load-fans! [] (reset! fans (read-string (slurp "data/fans.db"))))
+
+(save-celebs!)
+(save-fans!)
+(load-celebs!)
+(load-fans!)
 
 
-;; "http://api.thriftdb.com/api.hnsearch.com/items/_search?filter [fields] [username]=pg&pretty_print=true&filter [fields] [create_ts]= [2013-09-20T00:00:00Z%20+%20TO%20+%20*]"
 
 ;; http://api.thriftdb.com/api.hnsearch.com/items/_search?filter[fields][username]=pg&pretty_print=true&filter[fields][create_ts]=[2013-09-20T00:00:00Z%20+%20TO%20+%20*]
+;; (def hnurl "http://api.thriftdb.com/api.hnsearch.com/items/_search?filter[fields][username]=oskarth")
+;;"http://api.thriftdb.com/api.hnsearch.com/items/_search?sortby=create_ts=asc&limit=100&?filter[fields][username]=oskarth&filter[fields][create_ts]=[2013-01-01T00:00:00Z+TO+*]"
+;; http://api.thriftdb.com/api.hnsearch.com/items/_search?sortby=create_ts%20asc&filter[fields][username]=oskarth&filter[fields][create_ts]=[2013-09-20T00:00:00Z+TO+*]
 
-;; simple case: change pg to foo
-(def hnurl "http://api.thriftdb.com/api.hnsearch.com/items/_search?filter[fields][username]=oskarth")
+
 
 (defn hn-link [id name]
   (link-to (str "https://news.ycombinator.com/item?id=" id) name))
@@ -43,51 +62,59 @@
                              (get-in resi ["item" "title"]))
    [:br] [:br]))
 
-;; TODO: what about dates? item create_ts
 (defn acomment [res i]
-  (html5
-   [:div
+  [:div
     "by " (get-in (nth res i) ["item" "username"]) " | "
     (hn-link (get-in (nth res i) ["item" "id"]) "link")
 
     ;; if discussion is nil, it's a submission
     (if (nil? (get-in (res i) ["item" "discussion"]))
       (make-submission (nth res i))
-      (make-acomment (nth res i)))]))
+      (make-acomment (nth res i)))])
 
 (defn get-results [u]
   (get (parse-string (:body u))
-         "results"))
+       "results"))
 
+;; hits is total on server
+;; we can just do count right now
 ;; TODO: bug with indexoutofbounds
 (defn make-show [res]
-  (html5 (acomment res 0)
-            (acomment res 1)
-            (acomment res 2)))
+  (html5
+   [:br]
+   (for [n (range (count res))] (acomment res n))))
 
+;;(for [n (range 10)] (acomment res n))
+
+;; todo set date
 (defn httptest [q]
-  (let [u1 (http/get (str "http://api.thriftdb.com/api.hnsearch.com/items/_search?filter[fields][username]=" q))
+  (let [url1 "http://api.thriftdb.com/api.hnsearch.com/items/_search?sortby=create_ts%20desc&filter[fields][username]="
+        url2 "&filter[fields][create_ts]=[2013-09-25T00:00:00Z+TO+*]"
+        u1 (http/get (str url1 q url2))
+        ;;u1 (http/get (str "http://api.thriftdb.com/api.hnsearch.com/items/_search?filter[fields][username]=" q))
         u2 (http/get (str "http://api.thriftdb.com/api.hnsearch.com/items/_search?filter[fields][username]=" "pg"))]
     ;; Handle responses one-by-one, blocking as necessary
     ;; Other keys :headers :body :error :opts
     (println "response1's status: " (:status @u1))
     (println "response2's status: " (:status @u2))
-    (make-show (get-results @u1))))
+    (make-show (get (parse-string (:body @u1)) "results"))))
 
-(defn search-form []
+(defn search-form [q]
   (html5 (form-to [:POST "/search"]
-                 "Search "
-                 [:input {:type "text" :name "q" :value "" :size "17"}])))
+                  "Search "
+                  [:input {:type "text" :name "q" :value (or q "") :size "17"}])))
+
 
 (defn email-form [q]
   (html5
-   [:h2 "Get daily updates from " (str q) " in your mail."]
+   [:h1 "Get daily updates from " (str q) "."]
    (form-to [:POST "/assoc"]
-                 "Assoc email "
-                 [:input {:type "text" :name "e" :value "" :size "17"}])
-         (form-to [:POST "/dissoc"]
-                 "Dissoc email "
-                 [:input {:type "text" :name "e" :value "" :size "17"}])))
+                 "Email "
+                 [:input {:type "text" :name "e" :value "" :placeholder "" :size "17"}]
+                 [:input {:type "hidden" :name "q" :value (str q)}])
+         #_(form-to [:POST "/dissoc"]
+                  "Dissoc "
+                  [:input {:type "text" :name "e" :value "" :placeholder "test@example.com" :size "17"}])))
 
 (defn template [& body]
   (html5
@@ -96,22 +123,38 @@
     (include-css "css/style.css")]
    [:body
     [:h4.logo "Follow Hackers"]
-    [:p.byline "Made during Clojure Cup 2013"]
+    [:p.byline "Made during Clojure Cup 2013"] ;; TODO vote for us
     [:h1 "Search for hackers to follow."]
-    (search-form)
     [:br]
     body]))
 
 (defn index-page [q e]
   (template
+   (search-form q)
    (httptest q)
    [:br]
    (if (= q "") "" (email-form q))))
 
+;; TODO side effects
+(defn assoc-page [q e]
+  (println "subscribe e " e " to q " q) ;; db time
+  (str "You've subscribed to " q ". Check your inbox!"))
+
+;; TODO side effects
+(defn dissoc-page [q e]
+  (str "You've unsubscribed from " q "."))
+
+(defn dissoc-all-page [q e]
+  ;; TODO, POST form too
+  )
+
 (defroutes app-routes
   (GET "/" [q e] (index-page "" ""))
   (POST "/search" [q e] (index-page q ""))
-  (GET "/bar" [] (bar))
+  (POST "/assoc" [q e] (assoc-page q e))
+  (POST "/dissoc" [q e] (dissoc-page q e))
+  (GET "/dissoc-all" [q e] (dissoc-all-page q e))
+  (POST "/dissoc-all" [q e] (dissoc-all-page q e))
   (route/resources "/")
   (route/not-found "404"))
 
